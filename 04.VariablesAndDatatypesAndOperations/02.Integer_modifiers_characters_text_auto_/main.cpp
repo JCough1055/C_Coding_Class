7e01419453fbba108500d66f452d66fc9457d4cb
Jay <jcough1055@gmail.com>
2025-04-10 10:22:25 -0400

    We added code about integer modifiers. We concentrated oin signed and unsigned , short and long.

--- 04.VariablesAndDatatypesAndOperations/02.Integer_modifiers_characters_text_auto_/main.cpp
+++ 04.VariablesAndDatatypesAndOperations/02.Integer_modifiers_characters_text_auto_/main.cpp
@@ -1,36 +1,31 @@
 /*
-    . Topics: 
-        . Initialization: 
-            . Braced initialization
-            . Function style initialization
-            . Assignment initialization
-        . Integer modifiers
-        . Floating point numbers: 
-            . Precision
-            . Scientific notation
-        . Infinity and nan
-        . Characters and text
-        . Collections of characters
-            . std::vector
-            . std::array
-            . std::string
-        . auto
+   Topics:
+    . Initialization
+        - Braced Initialization
+        - Function style Initialization
+        - Assignment Initialization
+    . Integer modifiers
+    . Floating Point Numbers
+    . Precison
+    . Scientific Notation
+    . Infinity and nan
+    . Characters and text
+    . auto
+
 */
+#include<fmt/format.h>
 
-#include <fmt/format.h>
-
-// One line comment
 import utilities;
 
-int main() {
-    //braced_initialization();
-    //functional_initialization();
-    //assignment_initialization();
-    //integer_modifiers();
-    //floating_point_numbers();
-    //infinity_nan();
-    //bool_type();
-    //characters_and_text();
-    //collections_of_characters();
-    auto_keyword();
+int main(){
+    // Initialization
+
+    braced_initialization();
+    functional_initialization();
+    assignment_initialization();
+    integer_modifiers();
+
+
+    
+    return 0;
 }
