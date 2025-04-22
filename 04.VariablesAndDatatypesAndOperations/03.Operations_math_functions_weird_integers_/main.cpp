a209e056cafb0513bc317106f4fe3d104bc4344b
Jay <jcough1055@gmail.com>
2025-04-22 14:52:51 -0400

    These are changes to the

--- 04.VariablesAndDatatypesAndOperations/03.Operations_math_functions_weird_integers_/main.cpp
+++ 04.VariablesAndDatatypesAndOperations/03.Operations_math_functions_weird_integers_/main.cpp
@@ -3,7 +3,7 @@
         . Topic #1
         . Topic #2
 */
-
+#include <iostream>
 #include <fmt/format.h>
 #include <limits>
 #include<typeinfo>
@@ -20,7 +20,26 @@ int main(){
 //math_functions();
 //weird_integer_types();
 //conversions();
-overflow_underflow();
+//overflow_underflow();
+int number;
+std::cout<<"Enter a number: ";
+std::cin>>number;
+std::cout<<"You entered: "<<number<<std::endl;
+if (number%2 ==0){std::cout<<" Your number is even"<<std::endl;}
+else{std::cout<<"Your number is odd"<<std::endl;}
 
+double distance{0};
+    double horizontal{0};
+    double vertical{0};
+   double x1=1.0;
+   double x2=2.0;
+   double y1=4.0;
+    double y2=6.0;
+    horizontal= (x2-x1)*(x2-x1);
+    vertical= (y2-y1)*(y2-y1);
+    
+    distance= std::sqrt(horizontal+vertical);
+    
+    std::cout<<"The distance is :" <<distance<<std::endl;
 return 0;
 }
