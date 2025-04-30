e0ed089bd6b93bc1a6656740f127b4d24143bc3a
Jay <jcough1055@gmail.com>
2025-04-30 17:10:48 -0400

    added time and mont format and used ETC for time zone

--- 05.StreamsAndFormattingLiteralsConstants/03.Literals_and_constants_/main.cpp
+++ 05.StreamsAndFormattingLiteralsConstants/03.Literals_and_constants_/main.cpp
@@ -1,34 +1,22 @@
 /*
-    . Topics: 
-        . Literals
-        . Constants
-        . constexpr variables
-        . constexpr functions
-        . consteval functions
-        . constinit
+    . Project description
+        . Topic #1
+        . Topic #2
 */
-#include <fmt/format.h>
 
+#include <fmt/format.h>
 import utilities;
 
-//constinit : C++20. Guarantees that the variable is initialized at compile time.
-//Initialization with a run-time value will lead to a compiler error.
-const constinit int age{24};
-
-int randomizer = 5;
-//constinit int  car_count{add(3,randomizer)}; // Error. randomizer is not a compile time value
-
 int main(){
+    fmt::print("Hello, World!\n");
+    fmt::print("One\n");
+    fmt::print("Two\n");
+    fmt::print("Three\n");
+    int a=44;
+    int b=22;
+    int sum=a+b;
+    fmt::print("Sum of {} and {} is {}\n", a, b, sum);
 
-    //literals();
-    //constants();
-    //constexpr_variables();
-    //constexpr_functions();
-    //consteval_functions();
-
-    /*
-	//constinit double height{1.72}; //Error: 'height': 'constinit' only allowed on a variable 
-                                   //declaration with static or thread storage duration
-    */
     
+    return 0;
 }
