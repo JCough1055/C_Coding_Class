df8495526e361210e7cb2cb4655508b5a1a99613
Jay <jcough1055@gmail.com>
2025-05-14 22:58:49 -0400

    Addd code for random number generation

--- 07.Sequences/05.Random_numbers_/main.cpp
+++ 07.Sequences/05.Random_numbers_/main.cpp
@@ -1,19 +1,22 @@
 /*
-    Topics: 
-        . Random number generation old style
-        . Random number generation modern C++
-        . An example
+    . Topic Random Number Generation
+        . Generating random numbers using a seed timer (C++17)
+        . Generating random numbers the modern way with different libraries for generating numbers
 */
 
 #include <fmt/format.h>
+#include <climits>
+#include <cstdlib>
+#include <ctime>
 #include <iostream>
-#include <random>
-
+#include<random>
 import utilities;
 
 int main(){
-    //random_num_old_style();
-    //random_num_modern_cpp();
-    random_num_example();
+ 
+//random_number_old_style();
+//random_gen_modern_cpp();
+random_predictions();
 
+    return 0;
 }
