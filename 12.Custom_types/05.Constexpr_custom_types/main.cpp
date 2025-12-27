e0e552f7da39e25365d220152c6dc1482dd6ffc3
Jay <jcough1055@gmail.com>
2025-12-27 11:38:03 -0500

    Added code to make time available in constexpr and consteval in the code. Used classes in both types to use in main function

--- 12.Custom_types/05.Constexpr_custom_types/main.cpp
+++ 12.Custom_types/05.Constexpr_custom_types/main.cpp
@@ -1,22 +1,14 @@
 /*
-    . Constexpr and consteval classes: 
-
-        . The requirement for a class to be constexpr is that it provides: 
-            . A constexpr constructor
-        . Constexpr or consteval classes can have non constexpr member functions.
-            . see the print_time() function in the Time24 class.
-        . A constexpr function member function can't call a non-constexpr function.
-            . fmt::println is not constexpr in the fmt library.
-            . We can't call it from the print_time() function.
-        . The definitions of constexpr or consteval functions must be available in 
-            the module interface file. If you put it in the implementation file,
-            you may face issues.
+    . Project description
+        . Topic #1
+        . Topic #2
 */
+
+#include <fmt/format.h>
 
 import utilities;
 
-int main(){
-
-    use_time_demo();
-    
+int main() {
+print_current_time() ;
+    return 0;
 }
