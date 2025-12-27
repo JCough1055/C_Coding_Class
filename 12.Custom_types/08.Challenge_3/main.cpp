f715468b7b3d864bb80afa4dbf5216e0430b9488
Jay <jcough1055@gmail.com>
2025-12-27 14:50:06 -0500

    Added Code to make a grid of different colored boxes utilizing sfml library

--- 12.Custom_types/08.Challenge_3/main.cpp
+++ 12.Custom_types/08.Challenge_3/main.cpp
@@ -1,13 +1,21 @@
 /*
-    . Project description
-        . Topic #1
-        . Topic #2
+  A borad that acts s a starting 
+  Board Class represents a grid of cells
+  Each cell can be empty or occupied by a piece
+  The board should provide functionalities to:
+    - Initialize the board with a specific size
+    - Place a piece at a specific position
+    - Remove a piece from a specific position
+    - Check if a position is occupied
+    - Display the current state of the board
 */
 
 #include <fmt/format.h>
-#include <array>
+
+import utilities;
+
 
 int main() {
-
+app();
     return 0;
 }
